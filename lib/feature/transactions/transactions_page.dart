0919e715cfe475dbe232b73bdc511da37e34257c
SIKV <serhii.here@gmail.com>
2025-05-01 14:17:56 +0300

    Improve transactions page loading state

--- lib/feature/transactions/transactions_page.dart
+++ lib/feature/transactions/transactions_page.dart
@@ -7,6 +7,7 @@ import 'package:moneymanager/feature/transactions/extensions.dart';
 import 'package:moneymanager/feature/transactions/ui/transactions_list.dart';
 import 'package:moneymanager/navigation/routes.dart';
 import 'package:moneymanager/theme/assets.dart';
+import 'package:moneymanager/theme/dimens.dart';
 import 'package:moneymanager/ui/widget/collapsing_header_page.dart';
 import 'package:moneymanager/ui/widget/header_circle_button.dart';
 
@@ -22,69 +23,64 @@ class TransactionsPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final AppTheme appTheme = ref.watch(appThemeManagerProvider);
-    final headerState = ref.watch(headerControllerProvider);
+    final headerState = ref.watch(headerControllerProvider).value;
 
-    return headerState.when(
-      error: (_, __) {
-        // TODO: Implement.
-        return Container();
-      },
-      loading: () {
-        // TODO: Implement.
-        return Container();
-      },
-      data: (headerState) {
-        String title = headerState.amount;
-        String titleSuffix = ' ${headerState.currentWallet?.currency.symbol}';
+    String title = headerState?.amount ?? '...';
 
-        String filterTitle = getFilterTitle(context, headerState.rangeFilter, headerState.typeFilter);
+    String titleSuffix = headerState != null
+        ? ' ${headerState.currentWallet?.currency.symbol}'
+        : '';
 
-        String transactionsCount = Intl.plural(headerState.transactionsCount,
+    String filterTitle = headerState != null
+        ? getFilterTitle(context, headerState.rangeFilter, headerState.typeFilter)
+        : '';
+
+    String transactionsCount = headerState != null
+        ? Intl.plural(headerState.transactionsCount,
             zero: '${headerState.transactionsCount} ${AppLocalizations.of(context)!.lTransactions}',
             one: '${headerState.transactionsCount} ${AppLocalizations.of(context)!.lTransaction}',
             other: '${headerState.transactionsCount} ${AppLocalizations.of(context)!.lTransactions}',
-          );
+        )
+        : '';
 
-        return CollapsingHeaderPage(
-          colors: appTheme.colors,
-          startColor: appTheme.colors.transactionsHeaderStart,
-          endColor: appTheme.colors.transactionsHeaderEnd,
-          collapsedHeight: 78,
-          expandedHeight: 346,
-          title: title,
-          titleSuffix: titleSuffix,
-          subtitle: filterTitle,
-          tertiaryTitle: transactionsCount,
-          onTitlePressed: () {
-            _showChangeWallet(context);
-          },
-          primaryAction: IconButton(
-            icon: SvgIcon(Assets.filters,
-              size: 32,
-              color: appTheme.colors.alwaysWhite,
-            ),
-            onPressed: () => _showMenu(context),
-          ),
-          secondaryActions: [
-            HeaderCircleButton(
-              title: AppLocalizations.of(context)!.categoriesPage_title,
-              iconAsset: Assets.cart,
-              onPressed: () => Navigator.pushNamed(context, AppRoutes.categories),
-            ),
-            HeaderCircleButton(
-              title: AppLocalizations.of(context)!.statisticsPage_title,
-              iconAsset: Assets.chartVertical,
-              onPressed: () => Navigator.pushNamed(context, AppRoutes.statistics),
-            ),
-            HeaderCircleButton(
-              title: AppLocalizations.of(context)!.searchPage_title,
-              iconAsset: Assets.search,
-              onPressed: () => Navigator.pushNamed(context, AppRoutes.search),
-            ),
-          ],
-          sliver: const TransactionsList(),
-        );
+    return CollapsingHeaderPage(
+      colors: appTheme.colors,
+      startColor: appTheme.colors.transactionsHeaderStart,
+      endColor: appTheme.colors.transactionsHeaderEnd,
+      collapsedHeight: Dimens.transactionsPageCollapsedHeight,
+      expandedHeight: Dimens.transactionsPageExpandedHeight,
+      title: title,
+      titleSuffix: titleSuffix,
+      subtitle: filterTitle,
+      tertiaryTitle: transactionsCount,
+      onTitlePressed: () {
+        _showChangeWallet(context);
       },
+      primaryAction: IconButton(
+        icon: SvgIcon(Assets.filters,
+          size: 32,
+          color: appTheme.colors.alwaysWhite,
+        ),
+        onPressed: () => _showMenu(context),
+      ),
+      secondaryActions: [
+        HeaderCircleButton(
+          title: AppLocalizations.of(context)!.categoriesPage_title,
+          iconAsset: Assets.cart,
+          onPressed: () => Navigator.pushNamed(context, AppRoutes.categories),
+        ),
+        HeaderCircleButton(
+          title: AppLocalizations.of(context)!.statisticsPage_title,
+          iconAsset: Assets.chartVertical,
+          onPressed: () => Navigator.pushNamed(context, AppRoutes.statistics),
+        ),
+        HeaderCircleButton(
+          title: AppLocalizations.of(context)!.searchPage_title,
+          iconAsset: Assets.search,
+          onPressed: () => Navigator.pushNamed(context, AppRoutes.search),
+        ),
+      ],
+      sliver: const TransactionsList(),
     );
   }
 
--- lib/theme/dimens.dart
+++ lib/theme/dimens.dart
@@ -1,3 +1,6 @@
 class Dimens {
+  static const double transactionsPageCollapsedHeight = 78;
+  static const double transactionsPageExpandedHeight = 346;
+
   static const double changeWalletPageHeight = 124;
 }
